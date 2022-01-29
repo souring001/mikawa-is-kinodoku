7ca9320c79a569764cd1caaa594461f77646cf1e
aso <titechei@gmail.com>
2022-01-29 16:49:53 +0900

    さそり座の女以外の可能性を全部消してあげたい

---
+++ mikawa.js
@@ -0,0 +1,76 @@
+let 美川さん;
+const 星座リスト = ['おひつじ座', 'おうし座', 'ふたご座', 'かに座', 
+                  'しし座', 'おとめ座', 'てんびん座', 'さそり座', 
+                  'いて座', 'やぎ座', 'みずがめ座', 'うお座'];
+const 性別リスト = ['男', '女'];
+
+class Mikawa {
+  #名前;
+  #星座;
+  #性別;
+  #気持ちよく歌いだせるか;
+
+  constructor(名前, 星座, 性別) {
+    this.#名前 = 名前;
+    this.#星座 = 星座;
+    this.#性別 = 性別;
+    this.#気持ちよく歌いだせるか = false;
+  }
+
+  startIntro() {
+    console.log('~♪');
+  }
+
+  startSinging() {
+    if (this.#気持ちよく歌いだせるか) {
+      console.log('そうよ！私は'+this.#星座+'の'+this.#性別+'〜♪');
+    } else {
+      console.log('いいえ、私は'+this.#星座+'の'+this.#性別+'〜♪');
+      this.feelsSad(); // 落ち込んじゃう
+    }
+  }
+
+  isAsked(星座, 性別) {
+    if (this.#星座 == 星座 && this.#性別 == 性別) {
+      console.log('(頷く)');
+      this.#気持ちよく歌いだせるか = true;
+      return true;
+    } else {
+      console.log('(首を振る)');
+      return false;
+    }
+  }
+
+  feelsSad() {
+    return;
+  }
+
+  name() {
+    return this.#名前;
+  }
+}
+
+function init() {
+  美川さん = new Mikawa('美川', 'さそり座', '女');
+  美川さん.startIntro();
+  console.log('わー！'+美川さん.name()+'さんだー！！！'
+              +美川さん.name()+'さん！'+美川さん.name()+'さん！');
+}
+
+function search() {
+  let 星座;
+  let 性別;
+  let 返事 = false;
+
+  while(!返事) {
+    星座 = 星座リスト[Math.floor(Math.random() * 星座リスト.length)];
+    性別 = 性別リスト[Math.floor(Math.random() * 性別リスト.length)];
+  
+    console.log(星座+'の'+性別+'ですか？');
+    返事 = 美川さん.isAsked(星座, 性別);
+  }
+}
+
+init();
+search();
+美川さん.startSinging();
